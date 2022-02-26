9f3da1582f746918e327c722df1eb9f9c4398853
ahmedhawas <ahmedhawas7@gmail.com>
2022-02-26 12:24:39 -0500

    Add start and stop stream scripts to setup

---
+++ start_streaming.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gst-launch-1.0 rtspsrc location=rtsp://root:pass@${1}/axis-media/media.amp latency=150 ! queue ! rtph264depay ! queue ! h264parse ! flvmux streamable=true name=mux ! rtmpsink location="${2}" audiotestsrc ! voaacenc bitrate=128000 ! mux.
