946c40eb8c147a7149d673acb121696b25ddafad
Ahmed <ahmedhawas7@gmail.com>
2023-10-27 17:37:52 -0400

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -12,7 +12,7 @@ chmod +x stop-streaming.sh
 #Install and configure wireguard
 sudo apt update
 sudo apt install wireguard -y
-cd /etc/wireguard
+sudo cd /etc/wireguard
 wg genkey | sudo tee /etc/wireguard/private.key
 sudo chmod go= /etc/wireguard/private.key
 privkey=$(cat /etc/wireguard/private.key)
